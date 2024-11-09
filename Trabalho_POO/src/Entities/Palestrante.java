59f93d4ecb32aaa9b84a6dc809231450dcc2e1e5
Renan <114091580+RenanHdk@users.noreply.github.com>
2024-11-09 11:18:37 -0300

    Update Palestrante.java

--- Trabalho_POO/src/Entities/Palestrante.java
+++ Trabalho_POO/src/Entities/Palestrante.java
@@ -1,7 +1,5 @@
 package Entities;
 
-import java.util.ArrayList;
-
 public class Palestrante extends Pessoa{
     private String Especialidade;
     public Palestrante(String Nome, String Endereco, String Email, String Especialidade){
